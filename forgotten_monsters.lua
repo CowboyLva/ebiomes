b77cd87a29da0f890a084d25567a8ff915abfe8c
CowboyLv <108756200+CowboyLva@users.noreply.github.com>
2022-07-30 17:21:49 +0300

    Adding forgotten monsters support

---
+++ forgotten_monsters.lua
@@ -0,0 +1,96 @@
+--Forgotten monsters compatibility
+--For specified mobs(ones that dont spawn in a highly generalized way like in air or on stone)
+
+--hungry
+if not mobs.custom_spawn_monster then
+mobs:spawn({
+	name = "hungry:hungry",
+--	nodes = {"default:dirt_with_grass","default:dirt_with_rainforest_litter"},
+	nodes = {"ebiomes:dirt_with_grass_warm", "ebiomes:dirt_with_grass_cold", "ebiomes:dirt_with_grass_med", "ebiomes:dirt_with_grass_steppe_warm"},
+--	biomes = {"deciduous_forest_cold", "deciduous_forest_warm"},
+	min_light = 14,
+	--interval = 30, -- 60
+	chance = 9000,
+	min_height = 0,
+	max_height = 200,
+})
+end
+
+--skullmobs
+local fmeb = {
+"ebiomes:dirt_with_grass_cold",
+"ebiomes:dirt_with_grass_warm",
+"ebiomes:dirt_with_grass_med",
+"ebiomes:dirt_with_grass_swamp",
+"ebiomes:dry_dirt_with_grass_arid",
+"ebiomes:dry_dirt_with_grass_arid_cool",
+"ebiomes:dirt_with_grass_steppe_cold",
+"ebiomes:dirt_with_grass_steppe",
+"ebiomes:dirt_with_grass_steppe_warm"
+
+}
+
+--skull archers
+if not mobs.custom_spawn_monster then
+mobs:spawn({
+	name = "skullarchers:sarchers",
+	nodes = fmeb,
+	min_light = 0,
+	max_light = 14,
+	chance = 7000,
+	--min_height = 0,
+	--max_height = 200,
+	max_height = 200,
+})
+end
+
+--skulls
+if not mobs.custom_spawn_monster then
+mobs:spawn({
+	name = "skulls:skull",
+	nodes = fmeb,
+	min_light = 0,
+	max_light = 14,
+	chance = 7000,
+	--min_height = 0,
+	--max_height = 200,
+	max_height = 200,
+
+})
+end
+
+--skullswords
+if not mobs.custom_spawn_monster then
+mobs:spawn({
+	name = "skullsword:ssword",
+	nodes = fmeb,
+	min_light = 0,
+	max_light = 14,
+	chance = 7000,
+	--min_height = 0,
+	--max_height = 200,
+	max_height = 200,
+})
+end
+
+--spoky
+if not mobs.custom_spawn_monster then
+mobs:spawn({
+	name = "spoky:spoky",
+	nodes = fmeb,
+	min_light = 0,
+	max_light = 14,
+	interval = 60,
+	chance = 15000,
+	--min_height = 0,
+	--max_height = 200,
+	max_height = 200,
+
+})
+end
+
+
+
+
+
+
